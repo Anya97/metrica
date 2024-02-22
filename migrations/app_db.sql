889c96e82a863527c1ea3a82e7a1f3ce98f05de6
Anya <Galickaya_ane4ka@bk.ru>
2024-02-22 18:05:49 +0300

    fixed sql scripts

--- migrations/app_db.sql
+++ migrations/app_db.sql
@@ -1,6 +1,6 @@
 CREATE TABLE apps (
     id SERIAL PRIMARY KEY,
-    app_id uuid not null,
+    app_code uuid not null,
     user_code uuid not null unique,
     app_name VARCHAR (255) NOT NULL,
     app_token VARCHAR (255) NOT NULL,
@@ -8,14 +8,14 @@ CREATE TABLE apps (
     updated_at TIMESTAMP NOT NULL default now()
 );
 
-CREATE INDEX app_id_idx ON apps (app_id);
-CREATE INDEX user_id_idx ON apps (user_id);
+CREATE INDEX app_id_idx ON apps (app_code);
+CREATE INDEX user_id_idx ON apps (user_code);
 CREATE INDEX app_token_idx ON apps (app_token);
 
 
 CREATE TABLE events (
      id SERIAL PRIMARY KEY,
-     app_code uuid references apps(id),
+     app_code uuid references apps(app_code),
      event_code uuid not null,
      name VARCHAR (255) NOT NULL,
      description text,
@@ -24,6 +24,5 @@ CREATE TABLE events (
      updated_at TIMESTAMP NOT NULL default now()
 );
 
-CREATE INDEX app_id_idx ON events (app_id);
-CREATE INDEX args_idx ON events USING gin(args);
+CREATE INDEX app_id_idx ON events (app_code);
 CREATE INDEX name_idx ON events (name);
